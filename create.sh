6910fdb681c07b12fdfd82e2c0daaacb95997fdb
frankhan <hanrenda@megvii.com>
2020-03-31 17:52:20 +0800

    Tue Mar 31 17:52:20 CST 2020

--- create.sh
+++ create.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "create at:$(date)" > page.md
 cat head.md >> page.md
-cat body-0.md >> page.md
-cat body-1.md >> page.md
-cat body-2.md >> page.md
-cat body-3.md >> page.md
+cat body-0.md >> body.md
+cat body-1.md >> body.md
+cat body-2.md >> body.md
+cat body-3.md >> body.md
+./construct_000 body.md body0.md
+cat body0.md >> page.md
 cat tail.md >> page.md
-./construct_000 page.md page0.md
-rm page.md
+rm body.md body0.md
