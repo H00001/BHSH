da11f807ec986983fbc4420b46edd5a8fc5cbbf5
frankhan <hanrenda@megvii.com>
2020-04-01 09:25:17 +0800

    msge

--- create.sh
+++ create.sh
@@ -5,6 +5,7 @@ cat body-0.md >> body.md
 cat body-1.md >> body.md
 cat body-2.md >> body.md
 cat body-3.md >> body.md
+cat body-4.md >> body.md
 ./construct_000 body.md body0.md
 cat body0.md >> page.md
 cat tail.md >> page.md
