4583a8689733a61aaeae32e7e2b72ffb4db5a6f4
frankhan <hanrenda@megvii.com>
2020-04-02 18:18:40 +0800

    add sha256 supporto

---
+++ cd-help.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+i=0
+str=">"
+
+arr=("|" "/" "-" "\\")
+function show_process() {
+    while [[ $i -le $1 ]]
+    do
+    let index=i%4
+    let indexcolor=i%8
+    printf "\e[0;$2;1m[%-100s][%d%%]%c\e[0m %s\r" "$str" "$i" "${arr[${index}]}" "$4"
+    sleep $3
+    let i++
+    str='='${str}
+    done
+    if [[ $1 -eq 100 ]]; then
+        printf "\e[0m\n"
+    fi
+}
