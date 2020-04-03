20660fd3910202f8800603281c95905a90f9cd90
frankhan <hanrenda@megvii.com>
2020-04-03 11:38:23 +0800

    update time:Fri Apr  3 11:38:23 CST 2020

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ show_process 10 "31" 0.1 &
 git add . 1>/dev/null 2>&1
 show_process 20 "34" 0.2 &
 git commit -m "update time:$(date)" 1>/dev/null 2>&1
-show_process 40 "35" 0.3 
+show_process 40 "35" 0.5 & 
 git push 1>/dev/null 2>&1
 show_process 100 "36" 0.2
