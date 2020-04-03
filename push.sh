66d6ba470e41c93b0aeacfbaf39838adf9beb8de
frankhan <hanrenda@megvii.com>
2020-04-03 11:36:58 +0800

    update time:Fri Apr  3 11:36:58 CST 2020

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source ./cd-help.sh
-show_process 10 "31" 0.1
-git add .
-show_process 20 "34" 0.2
-git commit -m "update time:$(date)"
-show_process 40 "35" 0.3
-git push
+show_process 10 "31" 0.1 &
+git add . 1>/dev/null 2>&1
+show_process 20 "34" 0.2 &
+git commit -m "update time:$(date)" 1>/dev/null 2>&1
+show_process 40 "35" 0.3 
+git push 1>/dev/null 2>&1
 show_process 100 "36" 0.2
